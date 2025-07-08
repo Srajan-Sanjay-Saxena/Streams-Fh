35a6953af29ebe22b29ecc39d01f6b934af3e227
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2025-07-09 04:39:35 +0530

    added callbackWriting

--- callbackWriting.ts
+++ callbackWriting.ts
@@ -1,2 +1,19 @@
-import fs from "fs"
+import fs from "fs";
 
+(() => {
+  fs.open("./test.txt", "w", (err, fd: number) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.time("callbackWriting");
+    for (let i = 0; i < 1000000; i++) {
+        fs.write(fd, "a", () => {
+        });
+    }
+    console.timeEnd("callbackWriting");
+    fs.close(fd , () => {
+        console.log("Closed file...")
+    })
+  });
+})();
